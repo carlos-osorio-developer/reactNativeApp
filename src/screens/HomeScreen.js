ce152491804bbc3885c0b6988ef7037f2af26108
carlos-osorio-developer <osorio.uis@gmail.com>
2022-04-03 16:46:02 -0500

    Create the navigation Stack

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+      <Text>Estamos en HomeScreen</Text>
+    </View>
+  )
+}
